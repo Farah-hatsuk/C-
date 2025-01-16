6880da148649341c3598cacbed53e0a4d4071513
Farah-hatsuk <farah.hatsuk@gmail.com>
2025-01-16 16:18:02 +0300

    Add project files.

---
+++ C# task1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace C__task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Double
+            double A = 1.2;
+            console
+
+
+        }
+    }
+}
