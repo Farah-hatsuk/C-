7811c94d876cf689335b3dfa6fceffacd9957311
Farah-hatsuk <farah.hatsuk@gmail.com>
2025-01-21 21:26:44 +0300

    day 4

---
+++ class + objects/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class___objects
+{
+    internal class Program
+    {   // 1 
+
+        //class Hotel
+        //{
+        //    public int roomNumber;
+        //    public string roomType;
+        //    public int pricePerNight;
+        //    public string roomIsBook;
+
+        //    public const string HotelName = " Grand Stay Hotel";
+
+        //    public void print()
+        //    {
+        //        Console.WriteLine($" you have {roomIsBook} in {HotelName} and your room is {roomNumber} , type {roomType} the price for one night is {pricePerNight} . ");
+        //    }
+
+        //}
+
+        // 2 
+        class Hotel
+        {
+            public int RoomNumber { get; set; }
+            public string RoomType { get; set; }
+            public double PricePerNight { get; set; }
+            public bool RoomIsBook { get; set; }
+
+            public Hotel(int room_number, string room_type, double price_pernight, bool room_isbook)
+            {
+                RoomNumber = room_number;
+                RoomType = room_type;
+                PricePerNight = price_pernight;
+                RoomIsBook = room_isbook;
+
+            }
+
+            public Hotel() {
+                RoomNumber =1;
+                RoomType = " Single";
+                PricePerNight = 50.2;
+                RoomIsBook = true;
+
+            }
+
+
+
+            ~Hotel() {
+                Console.WriteLine("finish");
+            }
+
+        }
+            static void Main(string[] args)
+        {
+            //Hotel room1 = new Hotel();
+            //room1.roomNumber = 1;
+
+            //Console.WriteLine("choose your room type ( Single, Double, Suite ): ");
+            //string Type = Console.ReadLine();
+            //room1.roomType = Type;
+
+            //room1.pricePerNight = 50;
+
+            //room1.roomIsBook = "booked";
+
+            //Hotel room2 = new Hotel();
+            //room2.roomNumber = 2;
+
+            //Console.WriteLine("choose your room type ( Single, Double, Suite ): ");
+            //string Typee = Console.ReadLine();
+            //room2.roomType = Typee;
+
+            //room2.pricePerNight = 50;
+
+            //room2.roomIsBook = "not booked ";
+
+            //room1.print();
+
+            Hotel hotel = new Hotel();
+            Console.WriteLine(hotel.RoomType , hotel.RoomNumber , hotel.PricePerNight , hotel.RoomIsBook);
+            Hotel room1 = new Hotel(1 , "Double" , 50.2 , false);
+            Console.WriteLine(room1.RoomType + room1.RoomNumber + room1.PricePerNight + room1.RoomIsBook);
+
+            
+        }
+    }
+}
---
+++ day 4 (21-1)/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace day_4__21_1_
+{
+    internal class Program
+    {
+        class student                   // class 
+        {
+            private string name;                                 //field encapculation
+            private int age;
+            private int studentID;
+
+            const int minAge = 18;
+            const int maxAge = 40;
+            public string email {  get; set; }                     //property  
+
+            public string Name                                    //property 
+            {
+                get { return name; }
+                set { name = value; }
+            }
+
+            public int Age                                         //property 
+            {
+                get { return age; }                                 // check if the age is between 18 - 40 
+                set { if (value < minAge || value > maxAge)
+                    { 
+                        age = 20;
+                    } else
+                    {  age = value; }
+                }
+            }
+
+           public int StudentID                                    //property 
+            {
+                get { return studentID; }
+                set { studentID = value; }
+            }
+            
+            public student(string Name, int Age, int StudentID)                   // constructor with parameter 
+            {
+                this.Name = Name;                                                // this => for property    =   value for property 
+                this.Age = Age;
+                this.StudentID = StudentID;
+                
+
+            }
+
+            public void GetDetails()
+            {
+                Console.WriteLine($" {this.Name} , {this.Age} , {this.StudentID}");          // method 
+            }
+
+
+
+            ~student()                                                         // de constructor 
+            {
+                Console.WriteLine("Finish");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            student student1 = new student("Ahmad " , 22 , 1001);                         // make a object 
+
+            student1.GetDetails();                                                       // call the method 
+        }
+
+    }
+}
+
+
+//1 - What is class ?                             its a template 
+//2 - What is object  ?                           its a instance of class 
+//3 - Difference between class && object ?        we put the class above the nain   and the object under the main 
+//4 - Mention the OOP Principles  ?               Encapsulation / Inheritance / Polymorphism / Abstraction
+//5-	What is property                          its a way to control access to fildes inside a class by { get ; set ;}
+//6-	What is field                             editable data  بيانات يمكن تعديلها 
+//7-	What is constructor                       is a special method that used to initilize objects when we creat a class 
+//8-	What is encapsulation                     Encapsulation in C# is a mechanism that combines data and methods into a single unit called a class. + to make sure that "sensitive" data is hidden from users. 
+
+
